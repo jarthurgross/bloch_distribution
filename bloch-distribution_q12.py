bf0bebf9c696b0b4871609986513ce5ae67d4e58
Jonathan A. Gross <azaghalofbelegost@gmail.com>
2015-05-07 16:24:17 -0600

    Removed ticks and labels from axes

--- bloch-distribution_q12.py
+++ bloch-distribution_q12.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import matplotlib.pyplot as plt
 from matplotlib import cm, _cm, colors, gridspec, rcParams
+from matplotlib.ticker import NullLocator, NullFormatter
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from numpy import pi, sin, cos
@@ -8,8 +9,8 @@ from invert_angles import G_angles_q12
 from my_cms import husl_hot
 
 # Parameters
-N = 16
-epsilon = .5
+N = 2**8
+epsilon = 1
 
 Theta, Phi = np.mgrid[0:pi/2:complex(0, N), 0:2*pi:complex(0, 2*N + 1)]
 # Theta should be in the open interval (0, pi/2)
@@ -33,10 +34,10 @@ norm = colors.Normalize()
 
 cmap = husl_hot
 
-fontsize = 18
-fig = plt.figure(figsize=(18,6))
+fontsize = 72
+fig = plt.figure(figsize=(9,12))
 # fig = plt.figure()
-gs = gridspec.GridSpec(1, 2, width_ratios=[1, 3])
+gs = gridspec.GridSpec(2, 1, height_ratios=[3, 2])#, width_ratios=[5, 2])
 fig.suptitle(r'$\epsilon=' + str(epsilon) + '$', fontsize=fontsize)
 
 ax1 = plt.subplot(gs[0], projection='polar')
@@ -45,21 +46,31 @@ ax1 = plt.subplot(gs[0], projection='polar')
 ax1.pcolormesh(Phi, R, colorfunction, cmap=cmap, norm=norm,
                shading='gouraud', rasterized=True)
 ax1.set_ylim(0, np.max(R))
+ax1.xaxis.set_major_formatter(NullFormatter())
+ax1.yaxis.set_major_formatter(NullFormatter())
 # Having problems with my custom colormap and facecolors.
 ax2 = plt.subplot(gs[1], projection='3d')
 surf = ax2.plot_surface(X, Y, Z,  rstride=1, cstride=1,
                 facecolors=cmap(norm(colorfunction)), shade=False)
 surf.set_rasterized(True)
 ax2.set_zlim3d(0, 1)
+ax2.w_zaxis.set_major_formatter(NullFormatter())
+ax2.w_zaxis.set_major_locator(NullLocator())
 ax2.set_xlim3d(-1, 1)
+ax2.w_xaxis.set_major_formatter(NullFormatter())
+ax2.w_xaxis.set_major_locator(NullLocator())
 ax2.set_ylim3d(-1, 1)
+ax2.w_yaxis.set_major_formatter(NullFormatter())
+ax2.w_yaxis.set_major_locator(NullLocator())
+'''
 ax2.set_xlabel(r'$z$', fontsize=fontsize)
 ax2.set_ylabel(r'$x$', fontsize=fontsize)
 ax2.set_zlabel(r'$y$', fontsize=fontsize)
+'''
 m = cm.ScalarMappable(cmap=cmap, norm=norm)
 m.set_array(colorfunction)
-cbar = plt.colorbar(m)
+cbar = plt.colorbar(m, ticks=NullLocator(), format=NullFormatter())
 cbar.solids.set_edgecolor("face")
 #fig.colorbar(surf, shrink=0.5, aspect=10)
 rcParams['savefig.dpi'] = 300
-plt.savefig('plots/q12_bloch_e' + str(epsilon) + '.pdf')
+plt.savefig('plots/squint_talk_q12_bloch_e' + str(epsilon) + '.svg', transparent=True)
