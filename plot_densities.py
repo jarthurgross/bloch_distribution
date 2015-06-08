a164a4a063b0fc16300bbf7fb03781d4e0dbb775
Jonathan A. Gross <jarthurgross@gmail.com>
2015-06-08 07:07:41 -0600

    Modified to plot limit of densities at pole, but got it wrong...

--- plot_densities.py
+++ plot_densities.py
@@ -6,7 +6,6 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import h5py
 import argparse
-from my_cms import husl_hot
 
 if __name__ == '__main__':
 
@@ -16,23 +15,32 @@ if __name__ == '__main__':
                                      'Das-Arvind.')
     parser.add_argument('data_filenames', metavar='files', nargs='+',
                         help='List of hdf5 files containing densities')
+    parser.add_argument('--colormap', '-c', default='CMRmap',
+                        help='List of hdf5 files containing densities')
     args = parser.parse_args()
 
     data_file = h5py.File(args.data_filenames[0], 'r')
     epsilon = data_file['densities'].attrs['epsilon']
+    # The vstacks fill in the guessed limit of epsilon^2 for theta = 0
     Densities = data_file['densities'][:]
+    Densities = np.vstack((epsilon**2*np.ones(Densities.shape[1]), Densities))
     R = data_file['R'][:]
+    R = np.vstack((np.zeros(R.shape[1]), R))
     Phi = data_file['Phi'][:]
+    Phi = np.vstack((Phi[0,:], Phi))
     X = data_file['X'][:]
+    X = np.vstack((np.zeros(X.shape[1]), X))
     Y = data_file['Y'][:]
+    Y = np.vstack((np.zeros(Y.shape[1]), Y))
     Z = data_file['Z'][:]
+    Z = np.vstack((np.ones(Z.shape[1]), Z))
 
     mpl.rcParams['axes.labelsize'] = 72
     mpl.rcParams['axes.titlesize'] = 72
 
     norm = mpl.colors.Normalize()
 
-    cmap = husl_hot
+    cmap = plt.get_cmap(args.colormap)
 
     fig = plt.figure(figsize=(9,12))
     gs = mpl.gridspec.GridSpec(2, 1, height_ratios=[3, 2])
@@ -61,5 +69,5 @@ if __name__ == '__main__':
     ax2.w_yaxis.set_major_locator(NullLocator())
 
     mpl.rcParams['savefig.dpi'] = 300
-    plt.savefig('../../data/plots/bloch_densities_e' + str(epsilon) + '.pdf',
-                transparent=True)
+    plt.savefig('../../data/plots/bloch_densities_e' + str(epsilon) + '_' +
+                args.colormap + '.pdf', transparent=True)
