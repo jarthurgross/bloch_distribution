13e53716d5afa753946b8f4dfbb0e48942cc7657
Jonathan A. Gross <jarthurgross@gmail.com>
2015-06-22 16:40:31 -0600

    Generalized to plot multiple strengths and include axes

--- plot_densities.py
+++ plot_densities.py
@@ -2,10 +2,26 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.ticker import NullLocator, NullFormatter
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D, proj3d
+from matplotlib.patches import FancyArrowPatch
 import numpy as np
 import h5py
 import argparse
+
+class Arrow3D(FancyArrowPatch):
+    """From http://stackoverflow.com/a/11156353/1236650
+
+    """
+
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        FancyArrowPatch.draw(self, renderer)
 
 if __name__ == '__main__':
 
@@ -21,57 +37,109 @@ if __name__ == '__main__':
                         help='Extension for output file')
     args = parser.parse_args()
 
-    data_file = h5py.File(args.data_filenames[0], 'r')
-    epsilon = data_file['densities'].attrs['epsilon']
+    data_files = [h5py.File(data_filename, 'r') for data_filename in
+                  args.data_filenames]
+    epsilons = [data_file['densities'].attrs['epsilon'] for data_file in
+                data_files]
     # The vstacks fill in the indeterminate polar density with the average of
     # the neighboring densities
-    Densities = data_file['densities'][:]
-    Densities = np.vstack((np.mean(Densities[0,:]) *
-                           np.ones(Densities.shape[1]), Densities))
-    R = data_file['R'][:]
-    R = np.vstack((np.zeros(R.shape[1]), R))
-    Phi = data_file['Phi'][:]
-    Phi = np.vstack((Phi[0,:], Phi))
-    X = data_file['X'][:]
-    X = np.vstack((np.zeros(X.shape[1]), X))
-    Y = data_file['Y'][:]
-    Y = np.vstack((np.zeros(Y.shape[1]), Y))
-    Z = data_file['Z'][:]
-    Z = np.vstack((np.ones(Z.shape[1]), Z))
+    Density_meshes = [data_file['densities'][:] for data_file in data_files]
+    Density_meshes = [np.vstack((np.mean(Density_mesh[0,:]) *
+                                 np.ones(Density_mesh.shape[1]), Density_mesh))
+                      for Density_mesh in Density_meshes]
 
-    mpl.rcParams['axes.labelsize'] = 72
-    mpl.rcParams['axes.titlesize'] = 72
+    R_meshes = [data_file['R'][:] for data_file in data_files]
+    R_meshes = [np.vstack((np.zeros(R_mesh.shape[1]), R_mesh)) for R_mesh in
+                R_meshes]
+    Phi_meshes = [data_file['Phi'][:] for data_file in data_files]
+    Phi_meshes = [np.vstack((Phi_mesh[0,:], Phi_mesh)) for Phi_mesh in
+                  Phi_meshes]
+    X_meshes = [data_file['X'][:] for data_file in data_files]
+    X_meshes = [np.vstack((np.zeros(X_mesh.shape[1]), X_mesh)) for X_mesh in
+                X_meshes]
+    Y_meshes = [data_file['Y'][:] for data_file in data_files]
+    Y_meshes = [np.vstack((np.zeros(Y_mesh.shape[1]), Y_mesh)) for Y_mesh in
+                Y_meshes]
+    Z_meshes = [data_file['Z'][:] for data_file in data_files]
+    Z_meshes = [np.vstack((np.ones(Z_mesh.shape[1]), Z_mesh)) for Z_mesh in
+                Z_meshes]
+
+    fontsize = 24
+    mpl.rcParams['axes.labelsize'] = fontsize
+    mpl.rcParams['axes.titlesize'] = fontsize
 
     norm = mpl.colors.Normalize()
 
     cmap = plt.get_cmap(args.colormap)
 
-    fig = plt.figure(figsize=(9,12))
-    gs = mpl.gridspec.GridSpec(2, 1, height_ratios=[3, 2])
-    fig.suptitle(r'$\epsilon=' + str(epsilon) + '$')
+    fig = plt.figure(figsize=(2.5*(len(data_files) + 1),4))
+    gs = mpl.gridspec.GridSpec(2, len(data_files) + 1, height_ratios=[3, 2])
 
-    ax1 = plt.subplot(gs[0], projection='polar')
-    ax1.pcolormesh(Phi, R, Densities, cmap=cmap, norm=norm,
-                   shading='gouraud', rasterized=True)
-    ax1.set_ylim(0, np.max(R))
-    ax1.xaxis.set_major_formatter(NullFormatter())
-    ax1.yaxis.set_major_formatter(NullFormatter())
+    ax = plt.subplot(gs[0], aspect='equal', alpha=0, frame_on=False)
+    ax.set_xlim([-1.25, 1.25])
+    ax.set_ylim([-1.25, 1.25])
+    ax.xaxis.set_major_formatter(NullFormatter())
+    ax.xaxis.set_major_locator(NullLocator())
+    ax.yaxis.set_major_formatter(NullFormatter())
+    ax.yaxis.set_major_locator(NullLocator())
+    ax.arrow(-1, 0, 2, 0, width=.005, color='k')
+    ax.arrow(0, -1, 0, 2, width=.005, color='k')
+    ax.text(0.55, 1, r'$x$', fontsize=fontsize, horizontalalignment='left',
+            verticalalignment='top', transform=ax.transAxes)
+    ax.text(1, 0.55, r'$z$', fontsize=fontsize, horizontalalignment='right',
+            verticalalignment='bottom', transform=ax.transAxes)
 
-    # Having problems with my custom colormap and facecolors.
-    ax2 = plt.subplot(gs[1], projection='3d')
-    surf = ax2.plot_surface(X, Y, Z,  rstride=1, cstride=1,
-                            facecolors=cmap(norm(Densities)), shade=False)
-    surf.set_rasterized(True)
-    ax2.set_zlim3d(0, 1)
-    ax2.w_zaxis.set_major_formatter(NullFormatter())
-    ax2.w_zaxis.set_major_locator(NullLocator())
-    ax2.set_xlim3d(-1, 1)
-    ax2.w_xaxis.set_major_formatter(NullFormatter())
-    ax2.w_xaxis.set_major_locator(NullLocator())
-    ax2.set_ylim3d(-1, 1)
-    ax2.w_yaxis.set_major_formatter(NullFormatter())
-    ax2.w_yaxis.set_major_locator(NullLocator())
+    ax = plt.subplot(gs[len(data_files) + 1], projection='3d')
+    ax.set_zlim3d(0, 1)
+    ax.w_zaxis.set_major_formatter(NullFormatter())
+    ax.w_zaxis.set_major_locator(NullLocator())
+    ax.set_xlim3d(-1, 1)
+    ax.w_xaxis.set_major_formatter(NullFormatter())
+    ax.w_xaxis.set_major_locator(NullLocator())
+    ax.set_ylim3d(-1, 1)
+    ax.w_yaxis.set_major_formatter(NullFormatter())
+    ax.w_yaxis.set_major_locator(NullLocator())
+    x_arrow = Arrow3D([0,0],[-1,1],[0,0], mutation_scale=20, lw=2,
+                      arrowstyle='-|>', color='k')
+    y_arrow = Arrow3D([0,0],[0,0],[0,1], mutation_scale=20, lw=2,
+                      arrowstyle='-|>', color='k')
+    z_arrow = Arrow3D([-1,1],[0,0],[0,0], mutation_scale=20, lw=2,
+                      arrowstyle='-|>', color='k')
+    ax.add_artist(x_arrow)
+    ax.add_artist(y_arrow)
+    ax.add_artist(z_arrow)
+
+    ax.text(0, 1, 0, r'$x$', fontsize=fontsize)
+    ax.text(0.05, 0, 1, r'$y$', fontsize=fontsize)
+    ax.text(1, 0, 0.05, r'$z$', fontsize=fontsize)
+
+    for n in range(len(data_files)):
+        ax = plt.subplot(gs[n + 1], projection='polar')
+        ax.pcolormesh(Phi_meshes[n], R_meshes[n], Density_meshes[n], cmap=cmap,
+                      norm=norm, shading='gouraud', rasterized=True)
+        ax.set_ylim(0, np.max(R_meshes[n]))
+        ax.xaxis.set_major_formatter(NullFormatter())
+        ax.yaxis.set_major_formatter(NullFormatter())
+
+        ax.set_title(r'$\epsilon=' + str(epsilons[n]) + '$')
+
+        # Having problems with my custom colormap and facecolors.
+        ax = plt.subplot(gs[len(data_files) + 1 + n + 1], projection='3d')
+        surf = ax.plot_surface(X_meshes[n], Y_meshes[n], Z_meshes[n],
+                               rstride=1, cstride=1,
+                               facecolors=cmap(norm(Density_meshes[n])),
+                               shade=False)
+        surf.set_rasterized(True)
+        ax.set_zlim3d(0, 1)
+        ax.w_zaxis.set_major_formatter(NullFormatter())
+        ax.w_zaxis.set_major_locator(NullLocator())
+        ax.set_xlim3d(-1, 1)
+        ax.w_xaxis.set_major_formatter(NullFormatter())
+        ax.w_xaxis.set_major_locator(NullLocator())
+        ax.set_ylim3d(-1, 1)
+        ax.w_yaxis.set_major_formatter(NullFormatter())
+        ax.w_yaxis.set_major_locator(NullLocator())
 
     mpl.rcParams['savefig.dpi'] = 300
-    plt.savefig('../../data/plots/bloch_densities_e' + str(epsilon) + '_' +
+    plt.savefig('../../data/plots/bloch_densities_e' + str(epsilons[0]) + '_' +
                 args.colormap + '.' + args.filetype, transparent=True)
